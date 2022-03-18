c2540821ef85372e3af9dace542f54256e190156
Paulo Cabello Acha <pkbllo@yahoo.com>
2022-03-18 09:14:49 -0300

    First commit

---
+++ src/main/java/com/pauloapp/fundspringboot/FundspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pauloapp.fundspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundspringbootApplication.class, args);
+	}
+
+}
